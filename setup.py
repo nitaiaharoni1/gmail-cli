4593b71e223b9546da9e1d816a0271317445bcc6
nitaiaharoni <nitaiaharoni1@gmail.com>
2026-01-06 23:22:58 +0200

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gmail-cli",
-    version="1.2.0",
+    version="1.2.1",
     author="Nitai Aharoni",
     description="Command-line interface for Gmail",
     long_description=long_description,
